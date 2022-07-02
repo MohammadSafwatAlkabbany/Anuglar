320e7c6524a7303d31d7ff40ab8f27b67f80add2
Mohammad Safwat <85792194+MohammadSafwatAlkabbany@users.noreply.github.com>
2022-07-02 13:28:02 +0200

    Add files via upload

---
+++ Spring Boot/samplefiableApp/samplefiableApp/src/main/java/fiable/demo/samplefiableApp/SamplefiableAppApplication.java
@@ -0,0 +1,15 @@
+package fiable.demo.samplefiableApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplefiableAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SamplefiableAppApplication.class, args);
+		System.out.println("allah akbar");
+	}
+
+}
---
+++ Spring Boot/samplefiableApp/samplefiableApp/src/test/java/fiable/demo/samplefiableApp/SamplefiableAppApplicationTests.java
@@ -0,0 +1,13 @@
+package fiable.demo.samplefiableApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplefiableAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
